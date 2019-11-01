3a7ee64b5c8b497ad7fc3372b6d42bdedf9859d9
Evgeniy Kaptan <evgeniykaptan@gmail.com>
2019-11-01 14:51:13 +0200

    change home info

--- views/site/index.php
+++ views/site/index.php
@@ -11,41 +11,36 @@ $this->title = 'My Yii Test';
 
         <p class="lead">This page for football fans.</p>
 
-        <p><a class="btn btn-lg btn-success" href="https://football24.ua/ru/liga_chempionov_championat_tag50824/">Go with me</a></p>
+        <p><a class="btn btn-lg btn-success" href="https://www.uefa.com/uefachampionsleague/index.html">Go with me</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Goalkeepers</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Clean were few and far between on Matchday 3 as this round of fixtures featured the fewest shut-outs of the current UEFA Champions League campaign. Only six sides kept their opponents at bay and Paris are now the only team remaining who are yet to concede a goal in the competition – good news for owners of Keylor Navas (€5.5m) with the Costa Rican shot-stopper sitting on 18 #UCLfantasy points. However, this is not quite enough for top spot in the ranks as Dortmund's Roman Bürki (€5.5m) reigns supreme on 20 points, thanks in large part to his penalty save against Internazionale on Wednesday.
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+Overall, there have been 26 clean sheets across the first three matchdays, with an even distribution of 13 being kept at home and 13 away. There have been an increasing number of goals scored each matchday and with the business end of the group stage fast approaching, this trend could continue as teams fight to remain in the competition. Therefore, clean sheets may become even rarer as we progress, but it is notable that the top point-scoring goalkeepers are dominated by those who play for European heavyweights, with the likes of Manchester City's Ederson (€6.0m), Ajax keeper André Onana (€5.0m) and Atlético No1 Jan Oblak (€6.0m) all producing good returns so far..</p>
+
+                <p><a class="btn btn-default" href="https://www.uefa.com/uefachampionsleague/news/newsid=2630328.html?iv=true">Caylor Navas &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>football stars</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Cristiano Ronaldo and Lionel Messi have plenty of records already, but there are a few more targets to aim for. Cristiano Ronaldo and Lionel Messi's ongoing battle to be the top goalscorer in European football remains perhaps the sport's greatest drama of our age, but both still need to raise their game considerably if they are to scoop certain European Champion Clubs' Cup and UEFA Champions League records.
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+Messi became the first player to score in 15 consecutive UEFA Champions League campaigns (group stage to final) after he registered against Slavia Praha on Matchday 3, while Ronaldo set another record on 12 March when he became (at 34 years and 35 days) the oldest player to score a UEFA Champions League hat-trick, in a 3-0 Juventus win against Atlético Madrid. Can either of them break the following records?</p>
+
+                <p><a class="btn btn-default" href="https://www.uefa.com/uefachampionsleague/news/newsid=2232019.html?iv=true">Records Ronaldo and Messi have not broken&raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Match day goals</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Matchday 4 great goals down the years.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><a class="btn btn-default" href="https://www.uefa.com/uefachampionsleague/video/stars-of-ucl/videoid=2630342.html?iv=true">Great goals &raquo;</a></p>
             </div>
         </div>
 
